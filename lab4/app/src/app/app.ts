3b4e6a18bcd6992c28b34914141f60bb85b8b96d
Almaz <a_kurakov@kbtu.kz>
2026-02-18 12:40:08 +0500

    Control Flow in Components - @for

--- lab4/app/src/app/app.ts
+++ lab4/app/src/app/app.ts
@@ -2,16 +2,22 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template:`
-  @if (isServerRunning) {
-    <span>Yes, server is running</span>
-  }
-  @else {
-    <span>No, server is not running</span>
-  }
-  `
-
+  template: `
+  @for (user of users; track user.id) {
+    <p>{{ user.name }}</p>
+  }`,
 })
 export class App {
-  isServerRunning = true;
+  operatingSystems = [
+    {id: 'win', name: 'Windows'}, 
+    {id: 'osx', name: 'MacOS'}, 
+    {id: 'linux', name: 'Linux'}
+  ];
+  users = [
+  {id: 0, name: 'Sarah'},
+  {id: 1, name: 'Amy'},
+  {id: 2, name: 'Rachel'},
+  {id: 3, name: 'Jessica'},
+  {id: 4, name: 'Poornima'},
+  ];
 }
