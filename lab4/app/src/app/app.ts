e9bdca3c761e407392fbac4b7283f6a57c9a1296
Almaz <a_kurakov@kbtu.kz>
2026-02-18 11:58:21 +0500

    Updating the Component Class

--- lab4/app/src/app/app.ts
+++ lab4/app/src/app/app.ts
@@ -2,14 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-  Hello Universe
-`,
-  styles: `
-    :host {
-      color: blue;
-    }
-  `,
+  template: `Hello {{ city }}, {{1 + 1}}`,
 })
 export class App {
+  city = 'San Francisco';
 }
