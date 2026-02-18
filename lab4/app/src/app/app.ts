3131a3a26cb8732339a7f21b92d3cd274ddfefc9
Almaz <a_kurakov@kbtu.kz>
2026-02-18 23:04:27 +0500

    Component output properties

--- lab4/app/src/app/app.ts
+++ lab4/app/src/app/app.ts
@@ -1,9 +1,18 @@
 import {Component} from '@angular/core';
-import {User} from './user';
+import {Child} from './child';
 
 @Component({
   selector: 'app-root',
-  template: `<app-user occupation="Angular Developer"></app-user>`,
-  imports: [User],
+  template: `
+    <app-child />
+    <p>🐢 all the way down {{ items.length }}</p>
+  `,
+  imports: [Child],
 })
-export class App {}
+export class App {
+  items = new Array();
+
+  addItem(item: string) {
+    this.items.push(item);
+  }
+}
