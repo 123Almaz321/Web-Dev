580b4a81b4f04fc8fc246278749d0955f2194f85
Almaz <a_kurakov@kbtu.kz>
2026-02-18 13:04:18 +0500

    Event handling

--- lab4/app/src/app/app.ts
+++ lab4/app/src/app/app.ts
@@ -2,12 +2,24 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  styleUrls: ['app.css'],
-  template: ` 
-    <div contentEditable="false"><img alt="photo" [src]="imageURL" /></div> 
+  template: `
+    <section (mouseover) = "showSecretMessage()">
+      There's a secret message for you, hover to reveal 👀
+      {{ message }}
+    </section>
+
+    <button (click)="greet()" [textContent]="btnText">
   `,
 })
 export class App {
-  isEditable = true;
-  imageURL = "https://gstatic-wsc.olympics.com/games/7b2e170fbbde0f95e1a08fe2c9a26ce4/693c17986e6132d077dc97da_SquareIcon_bd0002b6-f29d-4e0a-8ec0-09933cd390d8.jpg";
+  message = '';
+  btnText = "hello"
+
+  showSecretMessage() {
+    this.message = 'Way to go 🚀';
+  }
+
+  greet() {
+    console.log('Hello, there 👋');
+  }
 }
