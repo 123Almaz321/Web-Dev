9883243c984e5b4bc6d6b1bd3f396d553f7dc013
Almaz <a_kurakov@kbtu.kz>
2026-02-18 23:29:17 +0500

    Routing Overview

--- lab4/app/src/app/app.ts
+++ lab4/app/src/app/app.ts
@@ -1,35 +1,15 @@
-import { Component } from '@angular/core';
-import { Comments } from './comments';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [Comments],
   template: `
-    <div>
-      <h1>How I feel about Angular</h1>
-      
-      <article>
-        <p>
-          Angular is my favorite framework, and this is why. Angular has the coolest deferrable view
-          feature that makes defer loading content the easiest and most ergonomic it could possibly be.
-        </p>
-        <p>
-          I can't express enough how much I enjoy working with Angular. It offers the best developer
-          experience I've ever had.
-        </p>
-        <p>Angular is great...</p><p>Angular is great...</p><p>Angular is great...</p>
-        <p>Angular is great...</p><p>Angular is great...</p><p>Angular is great...</p>
-      </article>
-
-      @defer (on viewport) {
-        <comments />
-      } @placeholder {
-        <p>Future comments</p>
-      } @loading (minimum 2s) {
-        <p>Loading comments...</p>
-      }
-    </div>
+    <nav>
+      <a href="/">Home</a>
+      |
+      <a href="/user">User</a>
+    </nav>
   `,
+  imports: [RouterOutlet],
 })
 export class App {}
