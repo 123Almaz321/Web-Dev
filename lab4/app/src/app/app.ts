042c8fdc4340de0a0528d7c468060637e74463ad
Almaz <a_kurakov@kbtu.kz>
2026-02-18 12:51:53 +0500

    Property Binding in Angular

--- lab4/app/src/app/app.ts
+++ lab4/app/src/app/app.ts
@@ -2,22 +2,12 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-  @for (user of users; track user.id) {
-    <p>{{ user.name }}</p>
-  }`,
+  styleUrls: ['app.css'],
+  template: ` 
+    <div contentEditable="false"><img alt="photo" [src]="imageURL" /></div> 
+  `,
 })
 export class App {
-  operatingSystems = [
-    {id: 'win', name: 'Windows'}, 
-    {id: 'osx', name: 'MacOS'}, 
-    {id: 'linux', name: 'Linux'}
-  ];
-  users = [
-  {id: 0, name: 'Sarah'},
-  {id: 1, name: 'Amy'},
-  {id: 2, name: 'Rachel'},
-  {id: 3, name: 'Jessica'},
-  {id: 4, name: 'Poornima'},
-  ];
+  isEditable = true;
+  imageURL = "https://gstatic-wsc.olympics.com/games/7b2e170fbbde0f95e1a08fe2c9a26ce4/693c17986e6132d077dc97da_SquareIcon_bd0002b6-f29d-4e0a-8ec0-09933cd390d8.jpg";
 }
